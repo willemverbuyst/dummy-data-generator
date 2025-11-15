9d59f1cc2c2d92e9f49fb5eb33331cccb78fc551
Willem <91901507+wverbuyst@users.noreply.github.com>
2025-11-15 05:05:51 +0100

    Add optimizeDeps configuration for testing libraries

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  optimizeDeps: {
+    include: ["@testing-library/react", "@testing-library/user-event"],
+  },
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
