27b0eff26e35e6d1ea3b597da529bd922436910d
Willem <91901507+wverbuyst@users.noreply.github.com>
2025-11-17 18:22:00 +0100

    Add babel-plugin-react-compiler to enhance React compilation

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,14 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react({
+      babel: {
+        plugins: ["babel-plugin-react-compiler"],
+      },
+    }),
+    tailwindcss(),
+  ],
   resolve: {
     alias: {
       "@/": new URL("./src/", import.meta.url).pathname,
